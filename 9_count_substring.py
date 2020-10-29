ae23dfdf42341b4f9d6a14cec64175838f7f1712
K V SAI JAYANTH KRISHNA <71439827+kvsaijayanthkrishna@users.noreply.github.com>
2020-10-29 09:16:01 +0530

    Write a Python program to count occurrences of a substring in a string.

---
+++ 9_count_substring.py
@@ -0,0 +1,5 @@
+#9. Write a Python program to count occurrences of a substring in a string.
+string=input("enter a string\t:")
+sub_string=input("enter a sub string\t:")
+n=string.count(sub_string)
+print("sub string occured %d times in main string"%n)
