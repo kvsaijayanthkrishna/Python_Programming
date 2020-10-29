d6566657557713cac40e219eb826a72e1b0c415b
K V SAI JAYANTH KRISHNA <71439827+kvsaijayanthkrishna@users.noreply.github.com>
2020-10-29 09:20:30 +0530

    Write a Python program to find the maximum character in a given string.

---
+++ 14_max_character.py
@@ -0,0 +1,4 @@
+#14. Write a Python program to find the maximum character in a given string. 
+string=input("enter a string\t:")
+max_character=max(string)
+print('maximum character in the "{0}" is {1}'.format(string,max_character))
