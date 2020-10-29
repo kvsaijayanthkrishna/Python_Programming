c92128ba088e95af8d878111f3c1433f58d221fd
K V SAI JAYANTH KRISHNA <71439827+kvsaijayanthkrishna@users.noreply.github.com>
2020-10-29 09:05:11 +0530

    Write a Python program to calculate the length of a string.

---
+++ 1_length_of_string.py
@@ -0,0 +1,5 @@
+#1. Write a python program to calculate the length of a string
+print("enter a string\t:",end="")
+string=input()
+length=len(string)
+print("length of {0} is {1}".format(string,length))
