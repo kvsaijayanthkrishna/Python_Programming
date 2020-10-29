0b448abd9ee036f5ebd47d9661c8856127ab8ab8
K V SAI JAYANTH KRISHNA <71439827+kvsaijayanthkrishna@users.noreply.github.com>
2020-10-29 09:15:13 +0530

    Write a Python script that takes input from the user and displays that input back in upper and lower cases.

---
+++ 8_string_upper_lower.py
@@ -0,0 +1,4 @@
+#.8. Write a Python script that takes input from the user and displays
+#that input back in upper and lower cases.
+script=input("copy paste any paragraph:\n")
+print("IN UPPER CASE\t:-",script.upper(),"in lower case\t:-",script.lower(),sep="\n")
